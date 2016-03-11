028b433c3d6cf51fd916f4d8647c94a58294dba2
Liangjie Hong <hongliangjie@gmail.com>
2016-03-11 11:45:23 -0800

    build options

--- Fugue.py
+++ Fugue.py
@@ -37,17 +37,9 @@ class cmdBuilder(object):
     def __init__(self, profile):
         self._profile = profile
         self._other_params = []
-        self._other_params.append('java')
-        self._other_params.append('-Xmx8192m')
-        self._other_params.append('-jar')
-        self._other_params.append('build/libs/fugue-topicmodeling-all-0.1.jar')
 
-    def run(self):
-        all_args = self._other_params
-        all_args.extend(self._profile.getParams())
-        print ' '.join(all_args)
+    def execute(self, all_args):
         fugue = subprocess.Popen(all_args, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
-
         while True:
             nextline = fugue.stdout.readline()
             if nextline == '' and fugue.poll() is not None:
@@ -61,12 +53,45 @@ class cmdBuilder(object):
             return output
         return
 
+    def run(self):
+        self._other_params.append('java')
+        self._other_params.append('-Xmx8192m')
+        self._other_params.append('-jar')
+        self._other_params.append('build/libs/fugue-topicmodeling-all-0.1.jar')
+        all_args = self._other_params
+        all_args.extend(self._profile.getParams())
+        cmdStr =  ' '.join(all_args)
+        print cmdStr
+        self.execute(all_args)
+
+class cmdCompile(cmdBuilder, object):
+    def __init__(self):
+        return
+
+    def run(self):
+        build_cmd = []
+        build_cmd.append('gradle')
+        build_cmd.append('build')
+        print '==================== Build ==================='
+        self.execute(build_cmd)
+        build_cmd = []
+        build_cmd.append('gradle')
+        build_cmd.append('fatJar')
+        print '==================== Jar ==================='
+        self.execute(build_cmd)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='The Fugue Topic Modeling Package')
     parser.add_argument('--profile', help = 'the profile to launch', default = 'ProfileAP')
+    parser.add_argument('--task', help = 'the task to perform', default = 'build')
     args = parser.parse_args()
-    if args.profile is not None and args.profile != '':
-        get_class = lambda x: globals()[x]
-        runProfile = get_class(args.profile)()
-        command = cmdBuilder(runProfile)
+    if args.task is not None and args.task != '':
+        if args.task == 'train':
+            if args.profile is not None and args.profile != '':
+                get_class = lambda x: globals()[x]
+                runProfile = get_class(args.profile)()
+                command = cmdBuilder(runProfile)
+        elif args.task == 'build':
+            command = cmdCompile()
         command.run()
